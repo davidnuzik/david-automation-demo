0cef8f67997de96b5d7c2be0e87a64643abaec9a
David Nuzik <david.nuzik@gmail.com>
2023-05-12 15:13:33 -0700

    Init
    * git init
    * Install Cypress
    * Add simple spec "first.spec.cy.js"
    * Add README.md
    
    Signed-off-by: David Nuzik <david.nuzik@gmail.com>

---
+++ README.md
@@ -0,0 +1,24 @@
+# David Nuzik's Automation Demo
+
+## About
+This project was created to demonstrate my ability to pick up a new testing framework and run with it.
+
+I have prior experience using JavaScript and TypeScript with Playwright, as well as other languages and frameworks.
+However, I have little or no experience working with the test frameworks demonstrated in this project.
+This project enables me to learn more about these test frameworks as well as showcase my knowledge and abilities.
+
+Please note that some or all specs may be heavily commented. This is simply for the sake of explaining the spec and/or
+what it does or does not do in order to showcase my understanding of the logic and what the spec accomplishes. I do not
+typically comment code except for in certain situations when a brief comment may be necessary to make it easier for the
+reader to grasp something. I generally prefer to read and write clean, simple, efficient, and effective code whenever
+possible.
+
+## How To Use This Project
+Each test framework is in its own folder at the root level, for example `cypress-demo`. Each framework is installed/
+initialized in a typical manner (such as what is recommended in the docs). The setup for each demo (config, location
+of files, folder structure, modules, etc) may not necessarily be conducive of a public or production-ready test suite,
+rather my intent is to show my ability to effectively design and code test cases and explain my rationale
+for each test.
+
+### Cypress Demo
+This framework was created with E2E tests which can be found at `./cypress-demo/cypress/e2e`
---
+++ cypress-demo/cypress/e2e/simple/first.spec.cy.js
@@ -0,0 +1,43 @@
+/**
+ * SUMMARY:
+ * This example is based on the "Writing Your First E2E Test" guide in the cypress.io docs.
+ * 
+ * This is a very simple test which navigates to David Nuzik's GitHub profile, searches (cy.contains) or gets specific
+ * text/elements, takes actions, and makes assertions. It shows some of the basic principles of visiting a page and
+ * then navigating it like a user may and making assertions.
+ * 
+ * REMARKS:
+ * Searching for strings and clicking in this manner ("/k3s") is not always an ideal setup. Depending on the app,
+ * there could be more than one result returned when searching for a specific string. As such, depending
+ * on how the page is designed and then rendered, unexpected circumstances could result in finding one of
+ * two or more results and clicking on the wrong one. If both/all the potential results are hyperlinked and
+ * navigate to the same location the test may still pass but may not be making the intended steps.
+ * 
+ * Therefore, when using selectors it is generally a good idea to provide more than one attribute or to be highly
+ * specific when selecting for a specific id or other content in the DOM. After the cy.contains you'll notice
+ * that I switch to getting elements by a specific id. This is more reliable and generally a better practice when
+ * it is an option. If it's necessary to use cy.contains to search for text you can use .first() or .last() or other
+ * means to differentiate between elements should more than one contain the specified string.
+ */
+
+describe('template spec', () => {
+  it('/davidnuzik - Simple assertions', () => {
+    // Visit /davidnuzik, check for "/k3s" string, click on the result
+    cy.visit('https://github.com/davidnuzik')
+    cy.contains('/k3s').click() //cy.get with specific search for id and/or attributes is a better practice (see remarks)
+
+    // Verify the URL should now include "/k3s"
+    cy.url().should('include', '/k3s')
+
+    // Get the "Issues" tab, click on it
+    cy.get('[id="issues-tab"]').click()
+
+    // Verify the URL should now include "/issues"
+    cy.url().should('include', '/issues')
+
+    // Get the search field, type into it
+    cy.get('[id="js-issues-search"]').type('author:davidnuzik')
+    // Verify the the expected value was appended to the pre-filled string
+    cy.get('#js-issues-search').should('have.value', 'is:issue is:open author:davidnuzik') // # is a shortcut for an id
+  })
+})
